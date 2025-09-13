280c3e58104985d26277fedc3a8cdbd85dc8608c
stewbagg <spencerdennison@proton.me>
2025-09-13 16:55:36 -0600

    /case command fully functional, move to wrapper next to reuse for other /commands

--- bot/commands/case.py
+++ bot/commands/case.py
@@ -1,17 +1,55 @@
 from bot.utils.db import pool
-from interactions import AutocompleteContext, Extension, OptionType, slash_command, slash_option, SlashContext
+from datetime import date
+from interactions import AutocompleteContext, Embed, EmbedAttachment, EmbedField, Extension, OptionType, slash_command, slash_option, SlashContext
 from psycopg.rows import class_row
 from pydantic import BaseModel
 
-class Cases(BaseModel):
+class Name(BaseModel):
     name: str
 
-# TODO: move to handler for reuse
+class Metadata(BaseModel):
+    name: str
+    id: int
+    release_date: date
+    icon_url: str
+    collection_name: str
+
+class Contents(BaseModel):
+    name: str
+    grade: str
+
+# TODO: move to wrapper for reuse
 async def get_cases(input):
-    async with pool.connection() as conn, conn.cursor(row_factory=class_row(Cases)) as cur:
-        await cur.execute("SELECT name FROM cases WHERE name ILIKE %s LIMIT 25", (f"%{input}%",))
-        cases = await cur.fetchall()
-        return cases
+    async with pool.connection() as conn, conn.cursor(row_factory=class_row(Name)) as cur:
+        await cur.execute("SELECT cases.name FROM cases WHERE cases.name ILIKE %s LIMIT 25", (f"%{input}%",))
+        names = await cur.fetchall()
+        return names
+
+async def get_case_metadata(name):
+    async with pool.connection() as conn, conn.cursor(row_factory=class_row(Metadata)) as cur:
+        await cur.execute("SELECT cases.id, cases.name, cases.release_date, cases.icon_url, cases.collection_id, collections.name AS collection_name FROM cases LEFT JOIN collections on cases.collection_id = collections.id WHERE cases.name = %s", (name,))
+        metadata = await cur.fetchone()
+        return metadata
+
+async def get_case_contents(id):
+    async with pool.connection() as conn, conn.cursor(row_factory=class_row(Contents)) as cur:
+        await cur.execute("SELECT name, grade FROM skins WHERE case_id = %s", (id,))
+        skins = await cur.fetchall()
+        return skins
+
+async def case_embed(name):
+    metadata = await get_case_metadata(name)
+    skins = await get_case_contents(metadata.id) # type: ignore
+    fields = []
+    for skin in skins:
+        fields.append(EmbedField(name = skin.name, value = skin.grade, inline = True))
+    embed = Embed(
+        title = metadata.name, # type: ignore
+        description = f"**Released:** {metadata.release_date.strftime('%m/%d/%Y')}\n**Collection**: {metadata.collection_name}\n**Skins**: {len(skins)}", # type: ignore
+        thumbnail = EmbedAttachment(url = metadata.icon_url), # type: ignore
+        fields = fields
+    )
+    return embed
 
 class Case(Extension):
     @slash_command(name="case", description="View a Counter-Strike Case")
@@ -23,7 +61,8 @@ class Case(Extension):
         autocomplete=True,
     )
     async def case(self, ctx: SlashContext, name: str):
-        await ctx.send()
+        embed = await case_embed(name)
+        await ctx.send(embed=embed)
     
     @case.autocomplete("name")
     async def case_autocomplete(self, ctx: AutocompleteContext):
